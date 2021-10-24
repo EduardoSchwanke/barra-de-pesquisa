3a3db07f3adfb3f4a4e65a4cf99f0171e101f242
Eduardo Schwanke <carvalhoe089@gmail.com>
2021-10-24 08:40:55 -0300

    elementos adicionados dinamicamente pelo JS

--- script.js
+++ script.js
@@ -1,8 +1,164 @@
 'use strict';
 
-const items = document.querySelectorAll('.item-single');
 const h1 = document.querySelector('.alert');
 let existeItem;
+
+const DB = [
+    {
+        'nome':'celular',
+        'valor':'800,00'
+    },
+    {
+        'nome':'computador',
+        'valor':'2000,00'
+    },
+    {
+        'nome':'tablet',
+        'valor':'500,00'
+    },
+    {
+        'nome':'HD',
+        'valor':'400,00'
+    },
+    {
+        'nome':'SSD',
+        'valor':'200,00'
+    },
+    {
+        'nome':'fonte',
+        'valor':'300,00'
+    },
+    {
+        'nome':'processador',
+        'valor':'500,00'
+    },
+    {
+        'nome':'monitor',
+        'valor':'200,00'
+    },
+    {
+        'nome':'Cabo Sata',
+        'valor':'10,00'
+    },
+    {
+        'nome':'cabo USB',
+        'valor':'15,00'
+    },
+    {
+        'nome':'TV',
+        'valor':'1400,00'
+    },
+    {
+        'nome':'MAC',
+        'valor':'1200,00'
+    },
+    {
+        'nome':'mouse',
+        'valor':'20,00'
+    },
+    {
+        'nome':'teclado',
+        'valor':'60,00'
+    },
+    {
+        'nome':'cooler',
+        'valor':'40,00'
+    },
+    {
+        'nome':'headset',
+        'valor':'200,00'
+    },
+    {
+        'nome':'celular',
+        'valor':'800,00'
+    },
+    {
+        'nome':'computador',
+        'valor':'2000,00'
+    },
+    {
+        'nome':'tablet',
+        'valor':'500,00'
+    },
+    {
+        'nome':'HD',
+        'valor':'400,00'
+    },
+    {
+        'nome':'SSD',
+        'valor':'200,00'
+    },
+    {
+        'nome':'fonte',
+        'valor':'300,00'
+    },
+    {
+        'nome':'processador',
+        'valor':'500,00'
+    },
+    {
+        'nome':'monitor',
+        'valor':'200,00'
+    },
+    {
+        'nome':'Cabo Sata',
+        'valor':'10,00'
+    },
+    {
+        'nome':'cabo USB',
+        'valor':'15,00'
+    },
+    {
+        'nome':'TV',
+        'valor':'1400,00'
+    },
+    {
+        'nome':'MAC',
+        'valor':'1200,00'
+    },
+    {
+        'nome':'mouse',
+        'valor':'20,00'
+    },
+    {
+        'nome':'teclado',
+        'valor':'60,00'
+    },
+    {
+        'nome':'cooler',
+        'valor':'40,00'
+    },
+    {
+        'nome':'headset',
+        'valor':'200,00'
+    }
+];
+
+
+const createElements = (nome, valor) => {
+    const item = document.createElement('div');
+    item.classList.add('item-single');
+    item.innerHTML = `
+        <p>${nome}</p>
+        <p>R$${valor}</p>
+    `;
+    document.querySelector('.items').appendChild(item);
+}
+
+const createAlert = () => {
+    const h1 = document.createElement('h1');
+    h1.innerHTML = 'Item não encontrado :(';
+    document.querySelector('.alert').appendChild(h1);
+}
+
+const showElements = () => {
+    DB.forEach(item => {
+        createElements(item.nome, item.valor)
+    })
+    createAlert();
+}
+
+showElements();
 
 const resetarItems = () => {
     items.forEach(item => {
@@ -21,7 +177,7 @@ const procurarItem = (itemDesejado) => {
     }else{ 
         resetarItems();
         items.forEach(item => {
-            let i = item.children[0].innerHTML
+            let i = item.children[0].innerHTML;
             item.style.display = 'none';
             if(i === itemDesejado){
                 existeItem = true;
@@ -41,6 +197,8 @@ const pesquisarItem = (event) => {
     const itemDesejado = document.querySelector('#pesquisa').value
     procurarItem(itemDesejado);
 }
+
+const items = document.querySelectorAll('.item-single');
 
 document.querySelector('.show-items').addEventListener('click', resetarItems)
 document.querySelector('form').addEventListener('submit', pesquisarItem);
